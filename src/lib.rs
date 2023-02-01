dc47dda00e30123866b49a97341b93e504ac4361
Fraser-MacColl <fraser.a.maccoll@gmail.com>
2023-02-01 18:51:28 +0000

    Added basic drawing to buffer capabilities

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@ pub use crossterm::style::{
     Color,
     Attribute, Attributes
 };
-
+use crossterm::style::{Print, PrintStyledContent, SetStyle};
 
 
 #[derive(Clone)]
-struct StyledChar {
+pub struct StyledChar {
     // Default is an empty (space) char with no colouring or attributes
     char: char,
     style: ContentStyle
@@ -54,7 +54,7 @@ pub struct TerminalRenderingEngine {
 }
 
 impl TerminalRenderingEngine {
-    fn new(position: (usize, usize), size: (usize, usize), clear_buffer: bool) -> TerminalRenderingEngine {
+    pub fn new(position: (usize, usize), size: (usize, usize), clear_buffer: bool) -> TerminalRenderingEngine {
         TerminalRenderingEngine {
             position,
             size,
@@ -64,17 +64,54 @@ impl TerminalRenderingEngine {
         }
     }
 
-    fn update_size(&mut self, size: (usize, usize)) {
+    pub fn update_size(&mut self, size: (usize, usize)) {
         todo!()
     }
 
-    fn update_pos(&mut self, pos: (usize, usize)) {
+    pub fn update_pos(&mut self, pos: (usize, usize)) {
         todo!()
     }
 
-    fn render(&self) {
+    pub fn render(&self) {
         todo!()
+    }
+
+    fn is_valid_pos(&self, pos: &(usize, usize)) -> bool {
+        // Validate pos is within area
+        // Don't need to check < as unsigned (no negatives)
+        if pos.0 >= self.size.0 { return false }
+        if pos.1 >= self.size.1 { return false }
+
+        true
     }
 
     //todo add various drawing methods (String, char, change region style etc)
+    pub fn draw_char(&mut self, pos: (usize, usize), char: char) {
+        if !self.is_valid_pos(&pos) { return; }
+        self.current_buffer[pos.0][pos.1] = StyledChar::from(char)
+    }
+
+    pub fn draw_styled_char(&mut self, pos: (usize, usize), styled_char: StyledChar) {
+        if !self.is_valid_pos(&pos) { return; }
+        self.current_buffer[pos.0][pos.1] = styled_char
+    }
+
+    pub fn draw_styled_string(&mut self, pos: (usize, usize), str: String, style: ContentStyle) {
+        // Pos is index to start inserting string from
+        // If string exceeds area, it is ignored
+        if !self.is_valid_pos(&pos) { return; }
+
+        let mut chars = str.chars();
+        for i in 0..str.len() {
+            // If there is a char
+            if let Some(c) = chars.next() {
+
+                // Don't bother with the rest
+                if pos.0+i >= self.size.0 { return; }
+
+                self.current_buffer[pos.0+i][pos.1] = StyledChar { char: c, style: style.clone() }
+
+            }
+        }
+    }
 }
